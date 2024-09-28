363a62910970979274b62518e7cd2c31f84e6d89
Lauren Pechey <118538771+pecheylauren02@users.noreply.github.com>
2024-09-28 14:54:58 +0400

    register admin models

--- school_lms/core/admin.py
+++ school_lms/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User, Student, Admin, Grade
+
+admin.site.register(User)
+admin.site.register(Student)
+admin.site.register(Admin)
+admin.site.register(Grade)
